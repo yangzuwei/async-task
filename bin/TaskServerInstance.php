f0fde641565bc88691c37bbcf266534ca3fb7ca2
wilson <316193839@qq.com>
2019-01-18 21:53:16 +0800

    fixed the start server autoload

--- bin/TaskServerInstance.php
+++ bin/TaskServerInstance.php
@@ -1,5 +1,12 @@
 <?php
-include dirname(__DIR__).'/src/Server/TaskServer.php';
-include dirname(__DIR__).'/src/DB/MedooPDO.php';
-include dirname(__DIR__).'/function/functions.php';
+//独立项目和集成项目自动加载器判断
+$autoloadPaths = [
+    dirname(__DIR__) . '/vendor/autoload.php',
+    dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php',
+];
+foreach ($autoloadPaths as $path) {
+    if (file_exists($path)) {
+        include $path;
+    }
+}
 (new \Wilson\Async\Server\TaskServer())->run();
