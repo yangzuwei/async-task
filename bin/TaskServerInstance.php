8f9e324f5a909759b8b536794297ba74b0b4eb79
wilson <316193839@qq.com>
2019-01-18 22:23:25 +0800

    rm pid file

--- bin/TaskServerInstance.php
+++ bin/TaskServerInstance.php
@@ -2,7 +2,7 @@
 //独立项目和集成项目自动加载器判断
 $autoloadPaths = [
     dirname(__DIR__) . '/vendor/autoload.php',
-    dirname(dirname(dirname(__DIR__))) . '/autoload.php',
+    dirname(dirname(dirname(dirname(__DIR__)))) . '/autoload.php',
 ];
 foreach ($autoloadPaths as $path) {
     if (file_exists($path)) {
