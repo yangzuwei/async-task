7942b0a27714cdbcef1243e85b500db755420f8b
wilson <316193839@qq.com>
2019-01-18 22:04:23 +0800

    fixed the start server autoload

--- function/functions.php
+++ function/functions.php
@@ -10,7 +10,7 @@ function getConfig()
 {
     if (defined('CI_VERSION')) {//CI框架配置读取
         $config = require APPPATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'swoole.php';
-    } elseif (function_exists('env')) { //laravel框架配置
+    } elseif (file_exists('.env')) { //laravel框架配置
         $config = require config_path('swoole.php');
     } else {//无框架使用
         $config = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'swoole.php';
